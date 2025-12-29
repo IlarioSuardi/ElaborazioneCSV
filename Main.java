e22170aa4367a0c447a51f3e77995722326440d7
suardi.22146 <suardi.ilario.studente@itispaleocapa.it>
2025-12-29 18:10:54 +0100

    Fine

--- Main.java
+++ Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            FileReader fr = new FileReader("suardi.csv");
+            String inputPath = "C:\\Users\\ilari\\OneDrive\\Desktop\\suardi.csv"
+                    ;
+            FileReader fr = new FileReader(inputPath);
             BufferedReader br = new BufferedReader(fr);
 
             Morte[] morti = new Morte[1000];
             int i = 0;
 
-            String line = br.readLine(); // intestazione
+            String line = br.readLine();
 
             while ((line = br.readLine()) != null && i < morti.length) {
                 String[] campi = line.split(",");
@@ -26,7 +28,7 @@ public class Main {
             }
 
             int max = 0;
-            for (int j=0; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (morti[j].toCSV().length() > max) {
                     max = morti[j].toCSV().length();
                 }
@@ -35,48 +37,43 @@ public class Main {
 
             System.out.println("\nANNO  STATO  DECESSI");
             for (int j = 0; j < 5 && j < i; j++) {
-                System.out.println(morti[j].year + " " + morti[j].state + " " + morti[j].deaths
-                );
+                System.out.println(morti[j].year + " " + morti[j].state + " " + morti[j].deaths);
             }
 
-            morti[i]=new Morte(new String[]{"2020","001","Cause Name","State","1000","10.0"});
+            morti[i] = new Morte(new String[]{"2020", "001", "Cause Name", "State", "1000", "10.0"});
             i++;
 
-            for (int j=0; j < i; j++) {
-                if(morti[j].year.equals("2010") && morti[j].state.equals("California")) {
+            for (int j = 0; j < i; j++) {
+                if (morti[j].year.equals("2010") && morti[j].state.equals("California")) {
                     System.out.println("Record trovato: " + morti[j].toCSV());
                 }
             }
 
             for (int j = 0; j < i; j++) {
-                if (morti[j].year.equals("2025") &&
-                        morti[j].state.equals("IT")) {
+                if (morti[j].year.equals("2025") && morti[j].state.equals("IT")) {
                     morti[j].deaths = "999";
-                }
-            }
-
-            for (int j = 0; j < i; j++) {
-                if (morti[j].year.equals("2025") &&
-                        morti[j].state.equals("IT")) {
                     morti[j].cancellato = "true";
                 }
             }
 
-            FileWriter fw = new FileWriter("output.csv");
+            String outputPath = "C:\\Users\\ilari\\OneDrive\\Desktop\\output.csv";
+            FileWriter fw = new FileWriter(outputPath);
             BufferedWriter bw = new BufferedWriter(fw);
 
             bw.write("Year,Cause113,CauseName,State,Deaths,Rate,MioValore,Cancellato");
             bw.newLine();
 
             for (int j = 0; j < i; j++) {
-                bw.write(String.format("%-120s", morti[j].toCSV()));
+                bw.write(morti[j].toCSV());
                 bw.newLine();
             }
 
             bw.close();
             fw.close();
-        }
-        catch (IOException e) {
+
+            System.out.println("File scritto correttamente in: " + outputPath);
+
+        } catch (IOException e) {
             System.out.println("Errore: " + e);
         }
     }
