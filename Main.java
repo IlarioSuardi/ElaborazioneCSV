17afcf26a403860358f0df17512e6dceaa568090
suardi.22146 <suardi.ilario.studente@itispaleocapa.it>
2025-12-29 17:15:02 +0100

    Struttura Classe Main

--- Main.java
+++ Main.java
@@ -2,5 +2,19 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        try{
+            FileReader fr = new FileReader("suardi.csv");
+            BufferedReader br = new BufferedReader(fr);
+
+            morte[] morti = new morte[1000];
+            int i = 0;
+
+            String line = br.readLine();
+            String line;
+            while((line = br.readLine()) != null && i < morti.length){
+                String[] campi = line.split(",");
+                morti[i] = new morte(campi);
+                i++;
+            }
     }
 }
