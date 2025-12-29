ec850f10aede334997c59cdf6f8d2b150db4e602
suardi.22146 <suardi.ilario.studente@itispaleocapa.it>
2025-12-29 17:41:16 +0100

    Visualizza 3 Campi

--- Main.java
+++ Main.java
@@ -2,18 +2,19 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        try{
+
+        try {
             FileReader fr = new FileReader("suardi.csv");
             BufferedReader br = new BufferedReader(fr);
 
-            morte[] morti = new morte[1000];
+            Morte[] morti = new Morte[1000];
             int i = 0;
 
-            String line = br.readLine();
-            String line;
-            while((line = br.readLine()) != null && i < morti.length){
+            String line = br.readLine(); // intestazione
+
+            while ((line = br.readLine()) != null && i < morti.length) {
                 String[] campi = line.split(",");
-                morti[i] = new morte(campi);
+                morti[i] = new Morte(campi);
                 i++;
             }
 
@@ -21,15 +22,25 @@ public class Main {
             fr.close();
 
             if (i > 0) {
-                System.out.println("Numero di Campi: " + morti[0].toCSV().split(",").length);
+                System.out.println("Numero di campi: " + morti[0].toCSV().split(",").length);
             }
 
-            int max =0;
+            int max = 0;
             for (int j = 0; j < i; j++) {
                 if (morti[j].toCSV().length() > max) {
                     max = morti[j].toCSV().length();
                 }
             }
-            System.out.println("Lunghezza Massima: " + max);
+            System.out.println("Lunghezza massima record: " + max);
+
+            System.out.println("\nANNO  STATO  DECESSI");
+            for (int j = 0; j < 5 && j < i; j++) {
+                System.out.println(morti[j].year + " " + morti[j].state + " " + morti[j].deaths
+                );
+            }
+
+        } catch (IOException e) {
+            System.out.println("Errore: " + e);
+        }
     }
 }
